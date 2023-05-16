5bc8eb04ceb6f3ba3e7de485218bbba6bff7061a
Aliaksei <vegas7546@.com>
2023-05-16 11:42:31 +0200

    Fix function

--- src/input.groovy
+++ src/input.groovy
@@ -3,3 +3,10 @@ def hasPermissionsDeploy() {
     message: "Deploy ${params.release} to Production?",
     webhookUrl: 'https://kaseya.webhook.office.com/webhookb2/2a46477e-0890-4d00-abe1-f653561dcc67@a1cd3436-6062-4169-a1bd-79efdcfd8a5e/IncomingWebhook/ee7a42daa2c143428b48696d8cb9e96d/7c0ecd83-6910-4f44-b219-c740a2fc2b5a'
   )
+  timeout(time: 30, unit: 'MINUTES') {
+    input(
+      message: 'Deploy to Production?',
+      submitter: 'noisecontrol'
+    )
+  }
+}
